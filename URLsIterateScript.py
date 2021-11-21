7ce3a5a1e744c6e2e51fd4d33bbf304650079748
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-11-21 12:02:12 -0500

    scripte

---
+++ URLsIterateScript.py
@@ -0,0 +1,47 @@
+try:
+    with open("ignores.url") as file:
+        datums = file.read().split("\n")
+except FileNotFoundError:
+    print("no ignores file")
+    datums = []
+
+sodium = []
+
+import sqlite3
+sqliteConnection = sqlite3.connect(input("DB filename? "))
+cursor = sqliteConnection.cursor()
+
+print("Connected to the DB!")
+
+a = cursor.execute(
+            """SELECT * FROM messages
+ORDER BY 1;"""
+)
+
+for i in a:
+    for j in i[3].split(" "):
+        if j.startswith("https://") and j not in datums and "\n" not in j:
+            datums.append(j)
+            sodium.append(j)
+            #print(j)
+print(len(datums), "urls processed.")
+
+print("Writing to file.")
+with open("ignores.url","w+") as ignrs:
+    hi = ""
+    for ignore in datums:
+        hi += ignore + "\n"
+    ignrs.write(hi)
+with open("urls.url","w+") as urls:
+    hi = ""
+    for url in sodium:
+        hi += url + "\n"
+    urls.write(hi)
+print("Written data.")
+print("Ignores stored in ignores.url")
+print("Urls stored in urls.url")
+print()
+
+sqliteConnection.close()
+
+print("Closed connection.")
